26be354d45cfa50a8e734266206378115dc4c153
abadon.gutierrez <abadon.gutierrez@gmail.com>
2015-01-05 23:17:52 -0600

    id:#17 - avoid using crypt in postgres
    Removed the usage of the crypt function in postgres db. This is a work
    in progress to migrate the app to use friend

--- migrations/src/main/resources/db/migration/V0.1.5.1__Test_migration_with_same_version.sql
+++ migrations/src/main/resources/db/migration/V0.1.5.1__Test_migration_with_same_version.sql
@@ -1,3 +1,3 @@
 -- This script didnt run with the version 1.5
 INSERT INTO public.nsp_user(username, email, password)
-    VALUES('trashuser', 'default@mail.com', crypt('password', gen_salt('bf')));
+    VALUES('trashuser', 'default@mail.com', 'password');
